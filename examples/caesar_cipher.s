ba3044aa831172a9e27e437de3bac8e4e45e6058
Jono Priestley <99158173+jonopriestley@users.noreply.github.com>
2023-09-19 17:29:40 +1000

    Fixed an issue with upper case encoding
    Upper case could go to lower case and be considered not changed so it wouldnt be correct before. fixed now

--- examples/caesar_cipher.s
+++ examples/caesar_cipher.s
@@ -1,7 +1,7 @@
 ;;; Data definitions go here
 .section .data
 to_encode:			; String to encode
-	.string "Message to encode\n"
+	.string "AVR to encode\n"
 positions:
 	.byte 12		; Number of positions to add
 	
@@ -75,15 +75,22 @@ encode:				; Subroutine to encode one letter
     in r30, 0x3d
     in r31, 0x3e
 
-    ldd r20, Z+3
+    ldd r20, Z+3	; r20 = letter value
+	
+	clr r16			; r16 = 0 -> lower case, r16 = 1 -> upper case
 
     ;; Jump to the end if the value is not a letter
     cpi r20, 0x41
     brlo do_nothing ;; do nothing if it's less than the capitals
-    cpi r20, 0x5b
-    brlo add_positions  ;; add positions if it's capital
-    cpi r20, 0x61
-    brlo do_nothing ; do nothing if it's between capitals and lower case
+    
+	cpi r20, 0x5b
+	brsh not_capital	;; skip to the next comparison if its not capital
+	ldi r16, 1			;; set the upper case marker
+    rjmp add_positions  ;; jump to add positions since it's capital
+    
+not_capital:
+	cpi r20, 0x61
+    brlo do_nothing ;; do nothing if it's between capitals and lower case
     cpi r20, 0x7b
     brsh do_nothing ;; do nothing if it's beyond lower case
     ;; else add positions cause it's lower case
@@ -98,7 +105,17 @@ add_positions:
     brlo do_nothing
     cpi r20, 0x61    ; check if the value is beyond capitals
     brlo sub_26
-    cpi r20, 0x7b    ; check if the value is with lower case
+	
+	;; If it was lower case then skip this. If it was upper case then check if it's gone beyond Z (0x5a)
+	cpi r16, 0
+	breq was_lower_case ; skip to lower case if it was lower case
+	
+	cpi r20, 0x5b
+	brsh sub_26
+	rjmp do_nothing
+	
+was_lower_case:
+    cpi r20, 0x7b    ; check if the value is within lower case
     brlo do_nothing
     ;; otherwise sub_26
 
