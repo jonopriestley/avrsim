e9ec95634fd1c05944ee3ccdcb91d35398839286
Jono Priestley <99158173+jonopriestley@users.noreply.github.com>
2024-05-29 16:17:04 +1000

    Change DMEM/PMEM bottom margins

--- support.js
+++ support.js
@@ -51,8 +51,7 @@ code_box.addEventListener('keydown', event => {
   }
 
   //else if (keys_down.length === 2 && keys_down.includes('Shift') && keys_down.includes('Tab')) {}
-}
-);
+});
 
 document.addEventListener('keyup', event => {
   // Remove key from list of key's down
@@ -72,75 +71,45 @@ code_box.addEventListener('keyup', event => {
   for (let i = 1; i < line_count + 1; i++) {
     lines_box.innerHTML += `${i}\n`;
   }
-}
-);
+});
 
 
 // Scroll both boxes
 code_box.addEventListener('scroll', event => {
-  
   const lines_box = document.getElementById('lines_box');
   lines_box.scrollTop = code_box.scrollTop;
-
-}
-);
-
-
+});
 
 const buttons = document.getElementsByClassName('button');
 
 // Button mouse enter
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('mouseenter', event => {
-    if (app.theme === 'light') {
-      buttons[i].style.backgroundColor = '#eee';
-      buttons[i].style.borderColor = '#d0d0d0';
-    } else {
-      buttons[i].style.backgroundColor = '#3e3e3e';
-      buttons[i].style.borderColor = '#5e5e5e';
-    }
-  }
-  );
+    buttons[i].style.backgroundColor = (app.theme === 'light') ? '#eee' : '#3e3e3e';
+    buttons[i].style.borderColor = (app.theme === 'light') ? '#d0d0d0' : '#5e5e5e';
+  });
 }
 
 // Button mouse leave
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('mouseleave', event => {
-    if (app.theme === 'light') {
-      buttons[i].style.backgroundColor = '#fff';
-      buttons[i].style.borderColor = '#e0e0e0';
-    } else {
-      buttons[i].style.backgroundColor = '#2e2e2e';
-      buttons[i].style.borderColor = '#4e4e4e';
-    }
-  }
-  );
+    buttons[i].style.backgroundColor = (app.theme === 'light') ? '#fff' : '#2e2e2e';
+    buttons[i].style.borderColor = (app.theme === 'light') ? '#e0e0e0' : '#4e4e4e';
+  });
 }
 
 // Button mouse down
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('mousedown', event => {
-    if (app.theme === 'light') {
-      buttons[i].style.backgroundColor = '#ddd';
-      buttons[i].style.borderColor = '#c0c0c0';
-    } else {
-      buttons[i].style.backgroundColor = '#4e4e4e';
-      buttons[i].style.borderColor = '#6e6e6e';
-    }
-  }
-  );
+    buttons[i].style.backgroundColor = (app.theme === 'light') ? '#ddd' : '#4e4e4e';
+    buttons[i].style.borderColor = (app.theme === 'light') ? '#c0c0c0' : '#6e6e6e';
+  });
 }
 
 // Button mouse up
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('mouseup', event => {
-    if (app.theme === 'light') {
-      buttons[i].style.backgroundColor = '#eee';
-      buttons[i].style.borderColor = '#d0d0d0';
-    } else {
-      buttons[i].style.backgroundColor = '#3e3e3e';
-      buttons[i].style.borderColor = '#5e5e5e';
-    }
-  }
-  );
+    buttons[i].style.backgroundColor = (app.theme === 'light') ? '#eee' : '#3e3e3e';
+    buttons[i].style.borderColor = (app.theme === 'light') ? '#d0d0d0' : '#5e5e5e';
+  });
 }
